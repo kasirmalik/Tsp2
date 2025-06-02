031d067050be6bf4d39ca8dfa3bb036b79b467f1
Kaiser Fayaz <kasirmalik87@gmail.com>
2025-06-02 09:37:24 +0530

    Add initial HTML and JavaScript/TypeScript files

---
+++ build/js/main.js
@@ -0,0 +1,7 @@
+"use strict";
+let userName = "John example";
+console.log(userName);
+let a = 12;
+let b = 6;
+let c = true;
+console.log(a / b);
---
+++ src/main.ts
@@ -0,0 +1,6 @@
+let userName:string  = "John example";
+console.log(userName)
+let a:number = 12;
+let b:number = 6;
+let c:boolean = true
+console.log(a / b)
