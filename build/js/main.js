7d6772e75353ef2ee728921cdfe8fc58d066c933
Kaiser Fayaz <kasirmalik87@gmail.com>
2025-06-03 09:26:09 +0530

    Refactor TypeScript code by commenting out example variables and functions for clarity

--- build/js/main.js
+++ build/js/main.js
@@ -1,7 +1,25 @@
 "use strict";
-let userName = "John example";
-console.log(userName);
-let a = 12;
-let b = 6;
-let c = true;
-console.log(a / b);
+// let userName:string  = "John example";
+// console.log(userName)
+// let a:number = 12;
+// let b:number = 6;
+// let c:boolean = true
+// console.log(a / b)
+// how typescript infers 
+// implict 
+let name2;
+name2 = "john";
+let meaninigOfLife;
+meaninigOfLife = 42;
+let isLoading;
+isLoading = true;
+let album; // any type of data // union type 
+album = 67;
+const sum = (a, b) => {
+    return a + b;
+};
+let postId;
+let isActive;
+let reg = /\w+/g;
+// explicit
+let name1 = "kasir";
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,31 @@
-let userName:string  = "John example";
-console.log(userName)
-let a:number = 12;
-let b:number = 6;
-let c:boolean = true
-console.log(a / b)
+// let userName:string  = "John example";
+// console.log(userName)
+// let a:number = 12;
+// let b:number = 6;
+// let c:boolean = true
+// console.log(a / b)
+
+// how typescript infers 
+// implict 
+let name2 ;
+name2 = "john"
+
+let meaninigOfLife :number;
+meaninigOfLife =42
+
+let isLoading:boolean;
+isLoading=true;
+
+let album: any; // any type of data // union type 
+album= 67
+
+const  sum =(a:number,b:string):number | string=> {
+    return a + b
+}
+let postId:string | number;
+let  isActive:number|boolean | string
+let reg:RegExp = /\w+/g
+
+
+// explicit
+let name1:string = "kasir"
