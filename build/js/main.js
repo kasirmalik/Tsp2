6deb3401c62c069524d837a15e59d6c78171c0ec
Kaiser Fayaz <kasirmalik87@gmail.com>
2025-06-04 21:42:33 +0530

    Refactor TypeScript code by adding type annotations, improving object structure, and implementing optional properties

--- build/js/main.js
+++ build/js/main.js
@@ -12,3 +12,48 @@ stringArr.push("e"); // adding new element to string array
 numberArr.push("7"); // adding new element to number array
 numberArr.unshift(0); // adding new element to beginning of number array
 mixedArr = stringArr; // adding new object to mixed array
+let test = []; // empty array type = any
+let bands = []; // string array
+let mixed = []; // mixed array with different types
+// tuple
+let myTuple = ["kasir", 1, true]; // tuple with string, number and boolean more strict than array
+let mixedt = ['john,', 1, true]; // mixed tuple with string, number and boolean
+mixedt = myTuple; // assigning tuple to mixed tuple
+// myTuple= mixedt // assigning mixed tuple to tuple will give error because mixed tuple is not strict
+let myobj;
+myobj = [];
+console.log(typeof myobj);
+myobj = bands;
+myobj = {};
+const exampleobj = {
+    prop1: 'Dave',
+    prop: true
+};
+exampleobj.prop = false;
+let evh = {
+    name: "array",
+    active: true,
+    album: [76, "kasir"]
+};
+let Jp = {
+    name: "array",
+    album: [89, "kasir", "loki"]
+};
+evh = Jp;
+// how to make properties optional
+const greetguitarist = (guitirist) => {
+    return `hello ${guitirist.name}`;
+};
+console.log(greetguitarist(Jp));
+let np = {
+    name: "kasir",
+    rollno: 87
+};
+const Glen = (gamer) => {
+    // type narrowing
+    if (gamer.rollno) {
+        return `Hello ${gamer.name.toUpperCase()}`;
+    }
+    return "Hello";
+};
+// Enums 
--- src/main.ts
+++ src/main.ts
@@ -23,3 +23,57 @@ let mixedt = ['john,', 1, true]; // mixed tuple with string, number and boolean
 mixedt = myTuple; // assigning tuple to mixed tuple
 // myTuple= mixedt // assigning mixed tuple to tuple will give error because mixed tuple is not strict
 
+let myobj:object
+myobj = []
+console.log(typeof myobj)
+
+myobj = bands
+myobj = {}
+
+const exampleobj = {
+    prop1 : 'Dave',
+    prop: true
+}
+exampleobj.prop = false
+
+type Guitarist = {
+    name:string,
+    active?:boolean,
+    album:(string | number)[]
+}
+
+let evh:Guitarist = {
+    name:"array",
+    active :true,
+    album:[76,"kasir"]
+}
+let Jp:Guitarist = {
+    name:"array",
+    album:[89,"kasir","loki"]
+}
+ evh = Jp
+ // how to make properties optional
+
+ const greetguitarist = (guitirist:Guitarist)=>{
+    return `hello ${guitirist.name}`
+ }
+ console.log(greetguitarist(Jp))
+
+ ///   interface it does same thing as type
+interface Gamer{
+    name:string,
+    rollno:number
+}
+let np:Gamer= {
+    name:"kasir",
+    rollno:87
+}
+const Glen=(gamer:Gamer)=>{
+    // type narrowing
+    if (gamer.rollno) {
+        return `Hello ${gamer.name.toUpperCase()}`
+    }
+    return "Hello"
+}
+
+// Enums 
