bcb95689125a069982b79b3f95ca30c0a31106aa
Kaiser Fayaz <kasirmalik87@gmail.com>
2025-06-04 09:16:36 +0530

    Refactor main.js by removing commented-out code and restoring array initialization and updates for clarity

--- build/js/main.js
+++ build/js/main.js
@@ -1,25 +1,14 @@
 "use strict";
-// let userName:string  = "John example";
-// console.log(userName)
-// let a:number = 12;
-// let b:number = 6;
-// let c:boolean = true
-// console.log(a / b)
-// how typescript infers 
-// implict 
-let name2;
-name2 = "john";
-let meaninigOfLife;
-meaninigOfLife = 42;
-let isLoading;
-isLoading = true;
-let album; // any type of data // union type 
-album = 67;
-const sum = (a, b) => {
-    return a + b;
-};
-let postId;
-let isActive;
-let reg = /\w+/g;
-// explicit
-let name1 = "kasir";
+let stringArr = ["a", "b", "c"]; // string array
+let numberArr = [1, 2, 3, "kasir"]; // number array
+let booleanArr = [true, false, true]; // boolean array
+// mixed array with different types
+// let mixedArr = [1, "a", true, { name: "kasir" }]; // mixed array with different types
+let mixedArr = [1, "a", true, { name: "kasir" }];
+stringArr[0] = "d"; // updating first element of string array
+numberArr[1] = 4; // updating second element of number array
+booleanArr[2] = false; // updating third element of boolean array
+stringArr.push("e"); // adding new element to string array
+numberArr.push("7"); // adding new element to number array
+numberArr.unshift(0); // adding new element to beginning of number array
+mixedArr = stringArr; // adding new object to mixed array
