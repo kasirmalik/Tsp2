47083ead877465f7cf79a8132391a720aee7d19b
Kaiser Fayaz <kasirmalik87@gmail.com>
2025-06-05 10:33:17 +0530

    Refactor main.js by removing unused arrays and variables, improving code clarity and organization

--- build/js/main.js
+++ build/js/main.js
@@ -1,59 +1,34 @@
 "use strict";
-let stringArr = ["a", "b", "c"]; // string array
-let numberArr = [1, 2, 3, "kasir"]; // number array
-let booleanArr = [true, false, true]; // boolean array
-// mixed array with different types
-// let mixedArr = [1, "a", true, { name: "kasir" }]; // mixed array with different types
-let mixedArr = [1, "a", true, { name: "kasir" }];
-stringArr[0] = "d"; // updating first element of string array
-numberArr[1] = 4; // updating second element of number array
-booleanArr[2] = false; // updating third element of boolean array
-stringArr.push("e"); // adding new element to string array
-numberArr.push("7"); // adding new element to number array
-numberArr.unshift(0); // adding new element to beginning of number array
-mixedArr = stringArr; // adding new object to mixed array
-let test = []; // empty array type = any
-let bands = []; // string array
-let mixed = []; // mixed array with different types
-// tuple
-let myTuple = ["kasir", 1, true]; // tuple with string, number and boolean more strict than array
-let mixedt = ['john,', 1, true]; // mixed tuple with string, number and boolean
-mixedt = myTuple; // assigning tuple to mixed tuple
-// myTuple= mixedt // assigning mixed tuple to tuple will give error because mixed tuple is not strict
-let myobj;
-myobj = [];
-console.log(typeof myobj);
-myobj = bands;
-myobj = {};
-const exampleobj = {
-    prop1: 'Dave',
-    prop: true
+// type alias
+// interface PostId = stringorNumber
+// litreal types 
+// to avoid  dry 
+let myNmae;
+// myNmae = 'john'
+let username;
+// username = 'kasir'
+// basic functions 
+const add = (a, b) => {
+    return a + b;
 };
-exampleobj.prop = false;
-let evh = {
-    name: "array",
-    active: true,
-    album: [76, "kasir"]
+// return not any thing it will give us void
+const logMsg = (message) => {
+    console.log(message);
 };
-let Jp = {
-    name: "array",
-    album: [89, "kasir", "loki"]
+logMsg('hello');
+logMsg(add(2, 4));
+let subtract = function (a, b) {
+    return a - b;
 };
-evh = Jp;
-// how to make properties optional
-const greetguitarist = (guitirist) => {
-    return `hello ${guitirist.name}`;
+let multiply = function (c, d) {
+    return c * d;
 };
-console.log(greetguitarist(Jp));
-let np = {
-    name: "kasir",
-    rollno: 87
-};
-const Glen = (gamer) => {
-    // type narrowing
-    if (gamer.rollno) {
-        return `Hello ${gamer.name.toUpperCase()}`;
+logMsg(multiply(2, 7));
+// optional params
+const addAll = (a, b, c) => {
+    // type Gaurd
+    if (typeof c != 'undefined') {
+        return a + b + c;
     }
-    return "Hello";
+    return a + b;
 };
-// Enums 
