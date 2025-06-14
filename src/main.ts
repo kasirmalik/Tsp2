341e6c33d5729caa7b179e3509da3c1ca7335138
Kaiser Fayaz <kasirmalik87@gmail.com>
2025-06-14 07:53:24 +0530

    Refactor main.ts by removing unused types and functions, improving code clarity and organization

--- src/main.ts
+++ src/main.ts
@@ -1,59 +1,11 @@
-// type alias
+const str = <T>(arg:T):T => arg
 
-type stringorNumber = string | number
-
-type stringorNumberarray = (string | number)[]
-
-type Globalist = {
-
+const isObj = <T>(arg:T):boolean=>{
+    return (typeof arg ==='object' && !Array.isArray(arg) && arg !== null)
 }
 
-type UserId = stringorNumber
+console.log(isObj(true))
+console.log(isObj("john"))
+console.log(isObj([1,2,3]))
+console.log(isObj(null))
 
-// interface PostId = stringorNumber
-
-// litreal types 
-// to avoid  dry 
-let myNmae : "kasir"
-// myNmae = 'john'
-
-let username : 'dave' | 'John' | 'amy'
-// username = 'kasir'
-
-// basic functions 
-
-const add = (a:number,b:number):number=>{
-    return a + b
-}
-// return not any thing it will give us void
-const logMsg = (message:any)=>{
-    console.log(message)
-}
-logMsg('hello');
-logMsg(add(2,4))
-
-
-let subtract = function (a:number,b:number):number{
-    return a-b
-}
-
-type mathFunction = (a:number,b:number)=> number
-
-let multiply : mathFunction = function(c,d){
-    return c * d
-}
-logMsg(multiply(2,7))
-
-interface gateFunsction {
-    (a:number,b:number):number
-}
-// optional params
-
-const addAll = (a:number,b:number,c?:number):number=>{
-    // type Gaurd
-    if (typeof c !='undefined') {
-         return a + b + c
-    }
-    return a + b
-   
-}
