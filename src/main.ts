626e56799bb01bed69e6ac0bae41f5af5163d0f6
Kaiser Fayaz <kasirmalik87@gmail.com>
2025-06-14 07:58:11 +0530

    Refactor isTrue function in main.ts to improve logic and clarity

--- src/main.ts
+++ src/main.ts
@@ -9,3 +9,15 @@ console.log(isObj("john"))
 console.log(isObj([1,2,3]))
 console.log(isObj(null))
 
+
+const isTrue = <T>(arg:T):{arg:T ,is:boolean}=>{
+    if (Array.isArray(arg) && !arg.length) {
+        return { arg, is: false }
+        
+        
+    }
+    if(isObj(arg) && !Object.keys(arg as keyof T)){
+        return { arg, is: false }
+    }
+    return { arg, is: true }
+}
